5f6dfda8e40aa74fd4da2684b46f79e58dcd9393
isssfire <22364591+isssfire@users.noreply.github.com>
2018-08-07 16:38:53 +0800

    Update RiceEdit2Servlet.java

--- src/com/wmsys/servlet/manager/RiceEdit2Servlet.java
+++ src/com/wmsys/servlet/manager/RiceEdit2Servlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.neusoft.services.RiceImpl;
+import com.wmsys.services.manager.RiceImpl;
 
 
 @WebServlet("/RiceEdit2Servlet")
@@ -24,11 +24,11 @@ public class RiceEdit2Servlet extends HttpServlet {
 		final RiceImpl services =new RiceImpl();
 		try 
 		{
-			//Ö²Èëdto
+			//æ¤å…¥dto
 			services.setMapDto(this.createDto(request));
 			//
 			
-			String msg = services.modify()?"ĞŞ¸Ä³É¹¦":"ĞŞ¸ÄÊ§°Ü";
+			String msg = services.modify()?"ä¿®æ”¹æˆåŠŸ":"ä¿®æ”¹å¤±è´¥";
 			request.setAttribute("msg", msg);
 				
 			
@@ -46,7 +46,7 @@ public class RiceEdit2Servlet extends HttpServlet {
 
 		this.doGet(request, response);
 	}
-	//µ¥Ò»ÊµÁ¦²éÑ¯
+	//å•ä¸€å®åŠ›æŸ¥è¯¢
 	private final void execute(RiceImpl services,HttpServletRequest request)throws Exception
 	{
 		Map<String,String> ins=services.findById();
@@ -57,29 +57,29 @@ public class RiceEdit2Servlet extends HttpServlet {
 		}
 		else
 		{
-			request.setAttribute("msg", "¸ÃÊı¾İÒÑÉ¾³ı»ò½ûÖ¹·ÃÎÊ!");
+			request.setAttribute("msg", "è¯¥æ•°æ®å·²åˆ é™¤æˆ–ç¦æ­¢è®¿é—®!");
 		}	
 	}
 
 
 
 
-//»ñÈ¡dto
+//è·å–dto
 	private final Map<String,Object> createDto(HttpServletRequest request)
   {
 		Map<String,String[]> tem=request.getParameterMap();
-		//1.»ñÈ¡ËùÓĞµÄ¼üÖµ¶Ô¼¯ºÏ(EntrySet)
+		//1.è·å–æ‰€æœ‰çš„é”®å€¼å¯¹é›†åˆ(EntrySet)
 		Set<Entry<String,String[]>> entrySet=tem.entrySet();
-		//¶¨Òå×Ö·û´®ÊıÖµ±íÊ¾EntryµÄvalue
+		//å®šä¹‰å­—ç¬¦ä¸²æ•°å€¼è¡¨ç¤ºEntryçš„value
 		String value[]=null;
-		//ÊµÀı»¯DTO
+		//å®ä¾‹åŒ–DTO
 		Map<String,Object> dto=new HashMap<>();
-		//Ñ­»·»ñÈ¡Ã¿¸ö¼üÖµ¶Ô
+		//å¾ªç¯è·å–æ¯ä¸ªé”®å€¼å¯¹
 		for(Entry<String,String[]>  entry:entrySet)
 		{
-			//»ñÈ¡¼üÖµ¶ÔµÄvalue²¿·Ö
+			//è·å–é”®å€¼å¯¹çš„valueéƒ¨åˆ†
 			value=entry.getValue();
-			//ÅĞ¶ÏvalueÊı×éµÄ³¤¶È,Èç¹ûÊÇ1,±íÊ¾¸Ã¿Ø¼şÊÇ·Çcheckbox,Èç¹û³¤¶È´óÓÚ1,±íÊ¾checkbox
+			//åˆ¤æ–­valueæ•°ç»„çš„é•¿åº¦,å¦‚æœæ˜¯1,è¡¨ç¤ºè¯¥æ§ä»¶æ˜¯écheckbox,å¦‚æœé•¿åº¦å¤§äº1,è¡¨ç¤ºcheckbox
 			if(value.length==1)
 			{
 				dto.put(entry.getKey(), value[0]);
